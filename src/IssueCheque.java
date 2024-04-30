7f2d37661cb227e1d2f83cadf8962b786cf27e14
Aayushman19 <112492292+Aayushman19@users.noreply.github.com>
2024-05-01 00:00:37 +0530

    Update IssueCheque.java

--- src/IssueCheque.java
+++ src/IssueCheque.java
@@ -3,8 +3,15 @@ import java.util.*;
 
 @SuppressWarnings("unused")
 public class IssueCheque extends ChequeStatus {
+    
+    public IssueCheque(int acc_no, String date) {
+        this.date = date;
+        this.acc_no = acc_no;
+    }
 
-    public IssueCheque() {
+    public int generateChequeNo(){
+        double c_no = Math.random()*1000000;
+        return (int)c_no;
     }
 
     public int cheque_no;
@@ -12,7 +19,10 @@ public class IssueCheque extends ChequeStatus {
     public int acc_no;
 
     public void issue_cheque() {
-        
+        int c_no = generateChequeNo();
+        System.out.println("Account number" + " " + acc_no);
+        System.out.println("Cheque generated with cheque number" + " " + c_no);
+        System.out.println("Date" + " " + ":" + " " + date);
     }
 
 }
