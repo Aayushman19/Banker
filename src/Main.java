ad9e407072ad7cd1a8df913a0998205166b0824a
Aayushman19 <112492292+Aayushman19@users.noreply.github.com>
2024-05-01 00:01:04 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.text.SimpleDateFormat;
 import java.util.*;
+
 
 // @SuppressWarnings("unused")
 public class Main {
     HashMap<Integer, ArrayList<String>>bankRecords = new HashMap<Integer, ArrayList<String>>();
     public static void main(String[] args) {
-        
+        SimpleDateFormat ft = new SimpleDateFormat("dd-MM-yyyy"); 
+        String str = ft.format(new Date());
+        IssueCheque ic = new IssueCheque(1001, str);
+        ic.issue_cheque();
     }
 }
