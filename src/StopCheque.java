ff30d19364ed8dd66b125880b000b551877db75b
Aayushman19 <112492292+Aayushman19@users.noreply.github.com>
2024-05-01 14:03:56 +0530

    Update StopCheque.java

--- src/StopCheque.java
+++ src/StopCheque.java
@@ -4,7 +4,9 @@ import java.util.*;
 @SuppressWarnings("unused")
 public class StopCheque extends ChequeStatus {
 
-    public StopCheque() {
+    public StopCheque(int acc_no, int cheque_no) {
+        this.acc_no = acc_no;
+        this.cheque_no = cheque_no;
     }
 
     public int acc_no;
@@ -13,7 +15,25 @@ public class StopCheque extends ChequeStatus {
     public String payee;
 
     public void stop_cheque() {
-        
+        System.out.println("Cancellation Request initiated for account number" + " " + acc_no);
+        System.out.println("Cheque number" + " " + cheque_no);
+
+        boolean valid = false;
+
+        for(int i=0 ; i<cheque_record.size() ; i++){
+            if(cheque_no == cheque_record.get(i)){
+                valid = true;
+            }
+        }
+
+        if(!valid){
+            System.out.println("Invalid Cheque number");
+        }
+
+        if(super.cheque_status_message == "Cheque Issued"){
+            super.cheque_status_message = "Issue Terminated and Refund initiated";
+            super.cheque_status();
+        }
     }
 
 }
