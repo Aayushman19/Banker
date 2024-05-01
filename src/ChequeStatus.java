6054d8aebb6efd4e8462d765119a89e44d655138
Aayushman19 <112492292+Aayushman19@users.noreply.github.com>
2024-05-01 14:05:26 +0530

    Update ChequeStatus.java

--- src/ChequeStatus.java
+++ src/ChequeStatus.java
@@ -1,9 +1,9 @@
-
 import java.util.*;
 
-@SuppressWarnings("unused")
+// @SuppressWarnings("unused")
 public class ChequeStatus {
-
+    public ArrayList<Integer> cheque_record = new ArrayList<Integer>();
+    
     public ChequeStatus() {
     }
 
@@ -12,7 +12,13 @@ public class ChequeStatus {
     public String cheque_status_message;
 
     public void cheque_status() {
-        
+        IssueCheque iCheque = new IssueCheque(cheque_no, cheque_date);
+        if (iCheque.issued) {
+            cheque_status_message = "Cheque Issued";
+        } else {
+            cheque_status_message = "Not Issued";
+        }
+        System.out.println(cheque_status_message);
     }
 
 }
