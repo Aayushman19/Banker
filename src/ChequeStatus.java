3b279a32709bacfb2eedb1f874a9bc004ed61f35
Aayushman19 <112492292+Aayushman19@users.noreply.github.com>
2024-05-02 10:07:58 +0530

    Update ChequeStatus.java

--- src/ChequeStatus.java
+++ src/ChequeStatus.java
@@ -1,15 +1,22 @@
 import java.util.*;
 
 // @SuppressWarnings("unused")
-public class ChequeStatus {
+public class ChequeStatus extends Main{
     public ArrayList<Integer> cheque_record = new ArrayList<Integer>();
     
     public ChequeStatus() {
+        super();
     }
 
     public int cheque_no;
     public String cheque_date;
     public String cheque_status_message;
+
+    public void printList(){
+        for(int i=0 ; i<cheque_record.size() ; i++){
+            System.out.println(cheque_record.get(i));
+        }
+    }
 
     public void cheque_status() {
         IssueCheque iCheque = new IssueCheque(cheque_no, cheque_date);
